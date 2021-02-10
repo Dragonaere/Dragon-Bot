fb7c807a5b0adf689a33c3dcfdce04ac3082ec0c
Dragonaere <43673678+Dragonaere@users.noreply.github.com>
2021-02-10 12:47:24 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from pretty_help import PrettyHelp, Navigation
+from discord.ext import commands
+
+from random_commands import Random
+from music_commands import Music
+
+orange = discord.Color.orange()
+
+bot = commands.Bot(':', description='Official Dragonaere Discord Bot')
+nav = Navigation()
+bot.help_command = PrettyHelp(navigation=nav, color=orange, active_time=5)
+bot.add_cog(Music(bot))
+bot.add_cog(Random(bot))
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="play.dragonaere.tk | :help"))
+    print('Logged in as:\n{0.user.name}\n{0.user.id}'.format(bot))
+
+bot.run(os.getenv("TOKEN"))
